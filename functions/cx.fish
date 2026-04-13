9f9089ec998a5c93f6ae49d1000007ac81fedc86
Ryan Hughes <ryan@heyoodle.com>
2026-04-13 17:28:09 -0400

    Update cx function

--- functions/cx.fish
+++ functions/cx.fish
@@ -1,4 +1,4 @@
-function cx --description 'Launch Claude Code in plan mode'
-  printf "\033[2J\033[3J\033[H"
-  claude --permission-mode=plan --allow-dangerously-skip-permissions $argv
+function cx --description 'Launch Claude Code with Dangerously Skip Permissions Allowed'
+    printf "\033[2J\033[3J\033[H"
+    claude --allow-dangerously-skip-permissions $argv
 end
