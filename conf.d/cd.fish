f76601b1700fb5301a41d07d418daa1f64c7b07a
Abdulrahman Ajlouni <ajlouni2000@gmail.com>
2026-05-05 03:33:05 +0300

    Fix zoxide cd wrapper history behavior

--- conf.d/cd.fish
+++ conf.d/cd.fish
@@ -1,8 +1,14 @@
+functions -c cd __original_cd
+
+function cd --wraps=zd --description 'alias cd=zd'
+  zd $argv
+end
+
 function zd --description 'zoxide-backed cd'
   if test (count $argv) -eq 0
-      builtin cd ~; and return
+      __original_cd ~; and return
   else if test -d $argv[1]
-      builtin cd -- $argv[1]
+      __original_cd -- $argv[1]
   else
       z $argv; and printf "\U000F17A9 "; and pwd || echo "Error: Directory not found"
   end
