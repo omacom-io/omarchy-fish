bc0efd7e1af7b37c4311e594de05da6bc271fbe5
Sebastian B <sb@wmc.sh>
2026-09-08 00:35:06 +0100

    mup: pull completions from mise up via --wraps
    Suggested in #10; matches the wrapper style of the single-command
    helpers.

--- functions/mup.fish
+++ functions/mup.fish
@@ -1,3 +1,3 @@
-function mup --description 'alias mup=MISE_MINIMUM_RELEASE_AGE=0 mise up'
+function mup --wraps='mise up' --description 'alias mup=MISE_MINIMUM_RELEASE_AGE=0 mise up'
     MISE_MINIMUM_RELEASE_AGE=0 mise up $argv
 end
